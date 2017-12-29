4927f297eba15eaf0c53f07b7fc25315550c2434
Andrew Aylett <andrew@aylett.co.uk>
2017-12-29 20:04:25 +0000

    Fix up the footer a bit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,8 +8,6 @@ import TwitterBoxIcon from 'mdi-react/TwitterBoxIcon';
 import FacebookBoxIcon from 'mdi-react/FacebookBoxIcon';
 
 import NavigationDrawer from 'react-md/lib/NavigationDrawers';
-import {Grid, Cell} from 'react-md/lib/Grids';
-import {Card, CardTitle, CardText} from 'react-md/lib/Cards';
 import {Chip} from 'react-md/lib/Chips';
 
 import module from './index.module.scss';
@@ -17,31 +15,6 @@ import module from './index.module.scss';
 import 'react-md/src/scss/_react-md.scss'
 import 'react-md/dist/react-md.blue-light_blue.min.css'
 import 'react-md/src/scss/_typography.scss'
-
-function getPageData(query) {
-    const thisPageSlug = query.file.relativePath;
-
-    switch (thisPageSlug) {
-        case '/':
-            return {
-                frontmatter: {
-                    title: "Home"
-                },
-                fields: {
-                    slug: '/'
-                }
-            };
-
-        default:
-            for (let node of query.allMarkdownRemark.edges) {
-                node = node.node;
-                if (node.fields.slug === thisPageSlug) {
-                    return node;
-                }
-            }
-            throw "Didn't find page in collection";
-    }
-}
 
 function getNavList(query) {
     const result = [
@@ -76,7 +49,23 @@ function getNavList(query) {
 }
 
 class ChipLink extends React.Component {
+    render() {
+        return (
+            <span>
+            <Link to={this.props.to}>
+                <Chip lassName={module.chiplink} label={this.props.label}
+                      avatar={this.props.avatar}/>
+            </Link>
+                &nbsp;
+            </span>)
+    }
 
+    static propTypes = {
+        to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired
+    }
+}
+
+class ChipA extends React.Component {
     render() {
         return (
             <span>
@@ -93,63 +82,6 @@ class ChipLink extends React.Component {
     }
 }
 
-const FooterCardOne = ()=>(
-    <Card className={Cell.getClassName({size: 4})}>
-        <CardTitle title='Get In Touch'/>
-        <CardText className={module.chipBlock}>
-            <ChipLink to='phone:+441316595602' avatar={<Avatar icon={<FontIcon>phone</FontIcon>}/>}
-                      label='(+44) 131 659 5602'/>
-            <ChipLink to='email:info@mannafields.org'
-                      avatar={<Avatar icon={<FontIcon>email</FontIcon>}/>}
-                      label='info@mannafields.org'/>
-            <ChipLink to='https://www.facebook.com/MannafieldsChristianSchool'
-                      avatar={<Avatar icon={<FacebookBoxIcon className={module.icon}/>}/>}
-                      label='Facebook'/>
-            <ChipLink to='https://twitter.com/mannafields'
-                      avatar={<Avatar icon={<TwitterBoxIcon className={module.icon}/>}/>}
-                      label='@mannafields'/>
-        </CardText>
-    </Card>
-);
-
-const FooterCardTwo = ()=>(
-    <Card className={Cell.getClassName({size: 4})}>
-        <CardTitle title='Footer Block Two'/>
-        <CardText>
-            <ChipLink to='phone:+441316595602' avatar={<Avatar icon={<FontIcon>phone</FontIcon>}/>}
-                      label='(+44) 131 659 5602'/>
-            <ChipLink to='email:info@mannafields.org'
-                      avatar={<Avatar icon={<FontIcon>email</FontIcon>}/>}
-                      label='info@mannafields.org'/>
-            <ChipLink to='https://www.facebook.com/MannafieldsChristianSchool'
-                      avatar={<Avatar icon={<FacebookBoxIcon className={module.icon}/>}/>}
-                      label='Facebook'/>
-            <ChipLink to='https://twitter.com/mannafields'
-                      avatar={<Avatar icon={<TwitterBoxIcon className={module.icon}/>}/>}
-                      label='@mannafields'/>
-        </CardText>
-    </Card>
-);
-
-const FooterCardThree = ()=>(
-    <Card className={Cell.getClassName({size: 4})}>
-        <CardTitle title='Footer Block Two'/>
-        <CardText>
-            <ChipLink to='phone:+441316595602' avatar={<Avatar icon={<FontIcon>phone</FontIcon>}/>}
-                      label='(+44) 131 659 5602'/>
-            <ChipLink to='email:info@mannafields.org'
-                      avatar={<Avatar icon={<FontIcon>email</FontIcon>}/>}
-                      label='info@mannafields.org'/>
-            <ChipLink to='https://www.facebook.com/MannafieldsChristianSchool'
-                      avatar={<Avatar icon={<FacebookBoxIcon className={module.icon}/>}/>}
-                      label='Facebook'/>
-            <ChipLink to='https://twitter.com/mannafields'
-                      avatar={<Avatar icon={<TwitterBoxIcon className={module.icon}/>}/>}
-                      label='@mannafields'/>
-        </CardText>
-    </Card>
-);
-
 export default ({children, data}) => {
     // const pageData = getPageData(data);
     return <div className={module.everything}>
@@ -165,11 +97,20 @@ export default ({children, data}) => {
             <div className={module.wrapper}>
                 {children()}
                 <div className={module.main}>
-                    <Grid>
-                        <FooterCardOne/>
-                        <FooterCardTwo/>
-                        <FooterCardThree/>
-                    </Grid>
+                    <ChipA to='tel:+441316595602' avatar={<Avatar icon={<FontIcon>phone</FontIcon>}/>}
+                           label='(+44) 131 659 5602'/>
+                    <ChipA to='mailto:info@mannafields.org'
+                           avatar={<Avatar icon={<FontIcon>email</FontIcon>}/>}
+                           label='info@mannafields.org'/>
+                    <ChipA to='https://www.facebook.com/MannafieldsChristianSchool'
+                           avatar={<Avatar icon={<FacebookBoxIcon className={module.icon}/>}/>}
+                           label='Facebook'/>
+                    <ChipA to='https://twitter.com/mannafields'
+                           avatar={<Avatar icon={<TwitterBoxIcon className={module.icon}/>}/>}
+                           label='@mannafields'/>
+                    <ChipLink to='/contact-us/'
+                              avatar={<Avatar icon={<FontIcon>school</FontIcon>}/>}
+                              label='Contact Us'/>
                 </div>
                 <div className={module.bottom}>
                     Copyright &copy; 2004-2017 Mannafields Christian Education Association. Scottish Charity No.
---
+++ src/pages/contact-us.md
@@ -0,0 +1,18 @@
+---
+title: Contact
+slug: contact-us
+index: 99
+subheader: true
+---
+
+The school postal address is:
+
+> MCEA Secretary \
+  Mannafields Christian School \
+  170 Easter Road \
+  Edinburgh \
+  EH7 5QE
+
+Tel: <a href="tel:+441316595602">(+44) 131 659 5602</a>
+
+Email: <a href="mailto:info@mannafields.org">info@mannafields.org</a>
