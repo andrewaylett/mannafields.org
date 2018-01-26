1fc5bd478d040b32dc61155e46681b2db85ecf92
Andrew Aylett <andrew@aylett.co.uk>
2018-01-26 16:41:36 +0000

    Revert "Disable offline and manifest"
    Also adds back in NPM packages, which were removed in a different
    commit.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,34 @@
 module.exports = {
     plugins: [
         {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "Mannafields Christian School",
+                short_name: "Mannafields",
+                start_url: "/",
+                background_color: "#2f4f63",
+                theme_color: "#2f4f63",
+                display: "browser",
+                icons: [
+                    {
+                        // Everything in /static will be copied to an equivalent
+                        // directory in /public during development and build, so
+                        // assuming your favicons are in /static/favicons,
+                        // you can reference them here
+                        src: `/favicons/logo-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `/favicons/logo-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
+            },
+        },
+        'gatsby-plugin-offline',
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `src`,
